da2ff36f1ff1dce8fe8276fdbf2e1768f332b348
Alexandr Grigorcea <alexandr.grigorcea@globalorange.nl>
2020-01-31 17:33:28 +0100

    updated keymaps

--- keyboards/40percentclub/5x5/keymaps/75ver/keymap.c
+++ keyboards/40percentclub/5x5/keymaps/75ver/keymap.c
@@ -59,33 +59,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
   [_QW] = LAYOUT_ortho_5x15(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_HOME, TD(_INS),KC_END,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGUP,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,            KC_LBRC, KC_RBRC, KC_BSLS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PGDN,
-  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,            KC_GRV,  TD(_MIN),TD(_EQL),KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            XXXXXXX, KC_UP,   KC_DEL,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
-  MO(_FN), KC_LGUI, KC_LALT, KC_LCPO, KC_SPC,  TD(SPC_ENT_SFT), KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_SPC,  KC_RCPC, KC_RALT, KC_RGUI, MO(_FN)
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,     KC_HOME, TD(_INS),KC_END,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PGUP,
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,     KC_LBRC, KC_RBRC, KC_BSLS,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PGDN,
+  KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_GRV,  TD(_MIN),TD(_EQL),  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     XXXXXXX, KC_UP,   KC_DEL,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, XXXXXXX,
+  MO(_FN), KC_LGUI, KC_LALT, KC_LCPO, KC_SPC,  MO(_FN),  KC_LEFT, KC_DOWN, KC_RGHT,   KC_BSPC, KC_SPC,  KC_RCPC, KC_RALT, KC_RGUI, TG(_FN)
   ),
 
 /* NUMBERS
  * .--------------------------------------------------------------------------------------------------------------------------------------.
- * |        | F1     | F2     | F3     | F4     | F5     |        |        |        | F6     | F7     | F8     | F9     | F10    | Brigt+ |
+ * |        | F1     | F2     | F3     | F4     | F5     |        |        |        | F6     | F7     | F8     | F9     | F10    | F11    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        | F11    | F12    |        |        |        |        |        |        |        |        |        |        | PrntSc | Brigt- |
+ * |        |        | Up     | Copy   | Insert |        |        |        |        |        |        |        |        | PrntSc | F12    |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        |        |        |        |        |        |        |        |        |        |        |
+ * |        | Left   | Down   | Right  | Enter  |        |        |        |        |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        |        | Play   | Vol+   | Mute   |        |        |        |        |        |        |
+ * |        |        |        |        | Tab    |        | Play   | Vol+   | Mute   |        |        |        |        |        |        |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
- * |        |        |        |        |        | Enter  | Prev   | Vol-   | Next   | Backsp |        |        |        |        |        |
+ * |        |        |        |        |        | Enter  | Prev   | Vol-   | Next   | Delete |        |        |        |        |        |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
   [_FN] = LAYOUT_ortho_5x15(
-  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BRIU,
-  _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_BRID,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, KC_MPLY, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, KC_ENT,  KC_MPRV, KC_VOLD, KC_MNXT, KC_BSPC, _______, _______, _______, _______, _______
+  _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+  _______, _______, KC_UP,  C(S(KC_C)), S(KC_INS), _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, KC_F12,
+  _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, KC_TAB, _______, KC_MPLY, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______,
+  _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT, KC_DEL, _______, _______, _______, _______, _______
   )
 };
 
--- keyboards/40percentclub/semaphore/keymaps/default/keymap.c
+++ keyboards/40percentclub/semaphore/keymaps/default/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_PGDN,
     KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-    MO(_FN), KC_LGUI, KC_LALT, KC_LCPO, KC_SPC,  TD(SPC_ENT_SFT),       KC_BSPC, KC_SPC,  KC_RCPC, KC_RALT, KC_RGUI, TG(_FN)
+    MO(_FN), KC_LGUI, KC_LALT, KC_LCPO, KC_SPC,  MO(_FN),       KC_BSPC, KC_SPC,  KC_RCPC, KC_RALT, KC_RGUI, TG(_FN)
   ),
 
   /* Lower
@@ -102,10 +102,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_FN] = LAYOUT_split( \
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                 KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-    _______, _______, KC_UP,   _______, _______, KC_GRV,                KC_LBRC, _______, _______, _______, KC_PSCR, KC_F12,
-    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_MINS,               KC_RBRC, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, KC_EQL,                KC_BSLS,  _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, KC_ENT,                KC_BSPC, KC_DEL,  _______, _______, _______, _______
+    _______, _______, KC_UP,   C(S(KC_C)), S(KC_INS), KC_GRV,                KC_LBRC, KC_HOME, _______, _______, KC_PSCR, KC_F12,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_ENT,  KC_MINS,               KC_RBRC, KC_END, _______, _______, _______, _______,
+    _______, _______, _______, _______, KC_TAB,  KC_EQL,                KC_BSLS,  _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______,               KC_DEL, _______,  _______, _______, _______, _______
   ),
 };
 
